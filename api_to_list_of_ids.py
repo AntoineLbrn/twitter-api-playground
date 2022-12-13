832c337e5f3979cbde009ffb2a4db0e082344b7e
AntoineLbrn <antoinelmh@hotmail.fr>
2022-12-13 08:39:05 +0100

    add mister mv

--- api_to_list_of_ids.py
+++ api_to_list_of_ids.py
@@ -1,9 +1,9 @@
 import json
 
 ids = []
-for i in range (1, 146):
-    with open(f'sly/data{i}.json', 'r') as f1:
+for i in range (1, 392):
+    with open(f'mv/data{i}.json', 'r') as f1:
         data1 = json.load(f1)
         ids = ids + [i["id"] for i in data1["data"]]
-with open('sly_followers_december_2022.json', 'w') as f2:
+with open('mv_followers_december_2022.json', 'w') as f2:
     json.dump(ids, f2)
